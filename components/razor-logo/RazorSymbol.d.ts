20c09d3df1e5ac97bfbbb9f248ff3cdfc8b8f889
Feras Ahmad <feras.ahmad@ippen-digital.de>
2024-11-24 16:53:07 +0100

    Updates

---
+++ components/razor-logo/RazorSymbol.d.ts
@@ -0,0 +1,3 @@
+import { ReactElement } from '../../../node_modules/react';
+import { IconProps } from '../../../../../../../src/components/types';
+export declare function RazorSymbol({ size, color, }: IconProps): ReactElement;
